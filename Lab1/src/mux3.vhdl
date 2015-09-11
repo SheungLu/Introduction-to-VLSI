aa8c47f9f0b84a88154b0bdb1afa0a12a126ecf8
sheunglu <slu2@umbc.edu>
2015-09-11 13:55:53 -0400

    mux

--- Lab1/src/mux3.vhdl
+++ Lab1/src/mux3.vhdl
@@ -1,8 +1,8 @@
 --
 -- Entity: mux3 
 -- Architecture : structural 
--- Author: cpatel2
--- Created On: 10/20/00 at 13:32
+-- Author: Sheung Lu
+-- Created On: 09/11/2015 1:55 PM
 --
 library STD;
 library IEEE;
@@ -12,8 +12,8 @@ entity mux3 is
 
 	port (
 		sel			:in		std_logic_vector(1 downto 0);
-		input		:in		std_logic_vector(3 downto 0);
-		output	:out	std_logic_vector(3 downto 0);
+		input		:in		std_logic;
+		output	:out	std_logic;
 		);
 end mux3;
 
@@ -30,4 +30,13 @@ for and2_1, and2_2: and2 use work.and2(structural);
 for or2_1: or2 use entity work.or2(structural);
 for inverter_1: inverter use entity work.inverter(structural);
 
+signal temp1, temp2, temp3: std_logic;
 
+begin
+
+and2_1: 	and2 	port map (sel(0), input, temp1);
+inverter_1: inverter port map (input, temp2);
+and2_2: 	and2 	port map (sel(1), temp2, temp3);
+or2_1:		or2		port map (temp1, temp3, output);
+
+end structural;
